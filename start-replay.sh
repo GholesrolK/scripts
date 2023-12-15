7c6cfcac149e250cc39e137df40e7c324164a3d7
GholesrolK <aelwa3r17@gmail.com>
2023-12-16 00:27:22 +0200

    w

---
+++ start-replay.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+video_path="$HOME/Videos/Recordings"
+mkdir -p "$video_path"
+gpu-screen-recorder -w focused -s 1920x1080 -f 60 -a "$(pactl get-default-sink).monitor" -c mp4 -r 90 -o "$video_path"
