c9c51a6584d664236d9ee700bd76b77e520a0197
GholesrolK <aelwa3r17@gmail.com>
2023-12-16 00:47:43 +0200

    added start replay

--- start-replay.sh
+++ start-replay.sh
@@ -2,4 +2,4 @@
 
 video_path="$HOME/Videos/Recordings"
 mkdir -p "$video_path"
-gpu-screen-recorder -w focused -s 1920x1080 -f 60 -a "$(pactl get-default-sink).monitor" -c mp4 -r 90 -o "$video_path"
+gpu-screen-recorder -w focused -s 1920x1080 -f 60 -a "$(pactl get-default-sink).monitor|$(pactl info | grep 'Default Source' | awk '{print $3}')" -c mp4 -r 90 -o "$video_path"
